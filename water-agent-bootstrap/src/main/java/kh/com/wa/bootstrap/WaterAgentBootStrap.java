847215c0cee6481110f08cfacc3f6a2912338ac9
Khang <mahkhang>
2021-12-19 17:02:23 +0700

    feat: first init back end structure [WA-7]

---
+++ water-agent-bootstrap/src/main/java/kh/com/wa/bootstrap/WaterAgentBootStrap.java
@@ -0,0 +1,12 @@
+package kh.com.wa.bootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterAgentBootStrap {
+    public static void main(String[] args) {
+        SpringApplication.run(WaterAgentBootStrap.class, args);
+    }
+
+}
