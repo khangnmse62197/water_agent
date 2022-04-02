70ea59b116c3a8623ccf43a9cfaa92521d76aa3f
Khang <mahkhang@gmail.com>
2022-04-02 09:13:30 +0700

    feat: [WA-14] make ping endpoint return some message, add test to ping endpoint

--- water-agent-rest/src/main/java/kh/com/wa/rest/controller/PingEndpoint.java
+++ water-agent-rest/src/main/java/kh/com/wa/rest/controller/PingEndpoint.java
@@ -1,16 +1,18 @@
 package kh.com.wa.rest.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
-@RequestMapping("/")
+@RequestMapping("/api")
 public class PingEndpoint {
-    @GetMapping("ping")
-    public void ping() {
+    @GetMapping("/ping")
+    public ResponseEntity<String> ping() {
         LOG.info("Ping endpoint reached");
+        return ResponseEntity.ok("pong");
     }
 }
