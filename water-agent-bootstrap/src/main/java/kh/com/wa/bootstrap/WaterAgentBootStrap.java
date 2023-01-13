e380a2beee33305feb43c1f42c0755c882941d22
Khang <khang.nguyen@mobi.ch>
2023-01-13 13:38:25 +0700

    feat: [WA-20] add missing flyway script

--- water-agent-bootstrap/src/main/java/kh/com/wa/bootstrap/WaterAgentBootStrap.java
+++ water-agent-bootstrap/src/main/java/kh/com/wa/bootstrap/WaterAgentBootStrap.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(WaterAgentRestConfiguration.class)
 public class WaterAgentBootStrap {
     public static void main(String[] args) {
         SpringApplication.run(WaterAgentBootStrap.class, args);
